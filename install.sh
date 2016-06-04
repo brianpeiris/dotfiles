1e18eeecb3fba690d231ee7a9f5d7e48ce5fddec
Brian Peiris <brianpeiris@gmail.com>
2016-06-03 17:34:44 -0700

    Tweaks. Add install script.

---
+++ install.sh
@@ -0,0 +1,38 @@
+# Install tmux
+pact install tmux
+
+# Install scm_breeze
+git clone git://github.com/ndbroadbent/scm_breeze.git ~/.scm_breeze
+~/.scm_breeze/install.sh
+
+# Install Vim Vundle
+if [ ! -e ~/.vim/bundle/Vundle.vim ]; then
+  git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+fi
+
+# Link vimrc
+script_path=$(dirname $0)
+if [ ! -e ~/.vimrc ]; then
+  ln -s $script_path/.vimrc ~/.vimrc
+fi
+
+# Install Vundle packages
+vim +PluginInstall +qa > /dev/null
+
+# Link tmux.conf
+if [ ! -e ~/.tmux.conf ]; then
+  ln -s $script_path/.tmux.conf ~/.tmux.conf
+fi
+
+rm ~/.gitconfig
+ln -s $script_path/.gitconfig ~/.gitconfig
+
+# Source zshrc
+if ! grep -q bp_zshrc ~/.zshrc; then
+  echo 'source ~/dotfiles/.bp_zshrc' >> ~/.zshrc
+fi
+
+# Add 'z' plugin
+if ! grep -q 'plugins+=(z)' ~/.zshrc; then
+  sed -i 's/^plugins=.\+/\0\nplugins+=(z)/' ~/.zshrc
+fi
